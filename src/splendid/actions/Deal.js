6c0754d6408b8caf34f26518363d8e2fbb56669f
Chris Smith <chris@chameth.com>
2023-08-06 00:15:44 +0100

    Don't pretend to deal from an empty deck

--- src/splendid/actions/Deal.js
+++ src/splendid/actions/Deal.js
@@ -4,8 +4,11 @@ export default {
     available: () => false,
 
     perform: function (state, {level}) {
-        const card = state.decks[level-1][0];
+        if (state.decks[level-1].length === 0) {
+            return [];
+        }
 
+        const card = state.decks[level-1][0];
         return [
             {
                 event: 'remove-card-from-deck',
