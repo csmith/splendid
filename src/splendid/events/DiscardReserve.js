15d65cf2702e5802100712f1df1c571ca8750ca9
Chris Smith <chris@chameth.com>
2023-08-05 16:50:35 +0100

    Handle discarding a reserve card when masked

--- src/splendid/events/DiscardReserve.js
+++ src/splendid/events/DiscardReserve.js
@@ -4,9 +4,18 @@ export default {
     name: 'discard-reserve',
 
     perform: (state, {playerId, card}) => {
-        state.players[playerId].reserved = _.filter(
-            state.players[playerId].reserved,
-            (c) => !_.isEqual(c, card)
-        );
+        if (state.players[playerId].reserved[0].bonus) {
+            // We have the full state, remove the correct card
+            state.players[playerId].reserved =
+                _.filter(
+                    state.players[playerId].reserved,
+                    (c) => !_.isEqual(c, card)
+                );
+        } else {
+            // We have a masked state, just pop any card off.
+            // TODO: Technically we should match the level, but nothing uses it at the minute..
+            state.players[playerId].reserved =
+                state.players[playerId].reserved.slice(1);
+        }
     }
 }
