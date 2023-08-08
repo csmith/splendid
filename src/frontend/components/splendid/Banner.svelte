9fea6ab8cb49682fa678c71cfafaa903d30e4f0c
Chris Smith <chris@chameth.com>
2023-08-08 12:30:38 +0100

    Let spectators see what's happening too...

--- src/frontend/components/splendid/Banner.svelte
+++ src/frontend/components/splendid/Banner.svelte
@@ -34,25 +34,21 @@
 </style>
 
 <section class:yours={state.turn === playerId}>
-  {#if !state.players[playerId]}
-    You are spectating.
-  {:else}
-    {#if state.turn === playerId}
-      It is your turn.
-    {:else if state.turn}
-      It is {state.players[state.turn].details.name}'s turn.
-    {/if}
+  {#if state.turn === playerId}
+    It is your turn.
+  {:else if state.turn}
+    It is {state.players[state.turn].details.name}'s turn.
+  {/if}
 
-    {#if state.phase === "setup"}
-      The game has not yet begun!
-    {:else if state.phase === "play"}
-      {pronoun} must take tokens, reserve a card, or buy a card.
-    {:else if state.phase === "discard"}
-      {pronoun} must discard excess tokens.
-    {:else if state.phase === "noble"}
-      {pronoun} must select a noble to receive.
-    {:else if state.phase === "end"}
-      Game over! {winnerNames()}
-    {/if}
+  {#if state.phase === "setup"}
+    The game has not yet begun!
+  {:else if state.phase === "play"}
+    {pronoun} must take tokens, reserve a card, or buy a card.
+  {:else if state.phase === "discard"}
+    {pronoun} must discard excess tokens.
+  {:else if state.phase === "noble"}
+    {pronoun} must select a noble to receive.
+  {:else if state.phase === "end"}
+    Game over! {winnerNames()}
   {/if}
 </section>
