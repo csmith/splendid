acb8676734d19726df354c241882c39be7c5594c
Chris Smith <chris@chameth.com>
2025-06-19 16:01:26 +0100

    Split out splendid events

---
+++ src/games/splendid/events.js
@@ -0,0 +1,35 @@
+import AddPlayer from "../shared/events/AddPlayer.js";
+import ChangePhase from "../shared/events/ChangePhase.js";
+import ChangePlayer from "../shared/events/ChangePlayer.js";
+import SetPlayerOrder from "../shared/events/SetPlayerOrder.js";
+import AddBonus from "./events/AddBonus.js";
+import AddPoints from "./events/AddPoints.js";
+import DiscardCard from "./events/DiscardCard.js";
+import DiscardReserve from "./events/DiscardReserve.js";
+import FinalRound from "./events/FinalRound.js";
+import PlaceCard from "./events/PlaceCard.js";
+import ReceiveNoble from "./events/ReceiveNoble.js";
+import RemoveCardFromDeck from "./events/RemoveCardFromDeck.js";
+import ReserveCard from "./events/ReserveCard.js";
+import ReturnTokens from "./events/ReturnTokens.js";
+import Setup from "./events/Setup.js";
+import TakeTokens from "./events/TakeTokens.js";
+
+export default [
+  AddBonus,
+  AddPlayer,
+  AddPoints,
+  ChangePhase,
+  ChangePlayer,
+  DiscardCard,
+  DiscardReserve,
+  FinalRound,
+  PlaceCard,
+  ReceiveNoble,
+  RemoveCardFromDeck,
+  ReserveCard,
+  ReturnTokens,
+  SetPlayerOrder,
+  Setup,
+  TakeTokens,
+];
--- src/games/splendid/game.js
+++ src/games/splendid/game.js
@@ -1,19 +1,4 @@
-import AddPlayer from "../shared/events/AddPlayer.js";
-import ChangePhase from "../shared/events/ChangePhase.js";
-import ChangePlayer from "../shared/events/ChangePlayer.js";
-import SetPlayerOrder from "../shared/events/SetPlayerOrder.js";
-import AddBonus from "./events/AddBonus.js";
-import AddPoints from "./events/AddPoints.js";
-import DiscardCard from "./events/DiscardCard.js";
-import DiscardReserve from "./events/DiscardReserve.js";
-import FinalRound from "./events/FinalRound.js";
-import PlaceCard from "./events/PlaceCard.js";
-import ReceiveNoble from "./events/ReceiveNoble.js";
-import RemoveCardFromDeck from "./events/RemoveCardFromDeck.js";
-import ReserveCard from "./events/ReserveCard.js";
-import ReturnTokens from "./events/ReturnTokens.js";
-import Setup from "./events/Setup.js";
-import TakeTokens from "./events/TakeTokens.js";
+import events from "./events.js";
 import phases from "./phases.js";
 import state from "./state.js";
 
@@ -32,22 +17,5 @@ export default {
     "sooner?",
   phases,
   state,
-  events: [
-    AddBonus,
-    AddPlayer,
-    AddPoints,
-    ChangePhase,
-    ChangePlayer,
-    DiscardCard,
-    DiscardReserve,
-    FinalRound,
-    PlaceCard,
-    ReceiveNoble,
-    RemoveCardFromDeck,
-    ReserveCard,
-    SetPlayerOrder,
-    Setup,
-    TakeTokens,
-    ReturnTokens,
-  ],
+  events,
 };
